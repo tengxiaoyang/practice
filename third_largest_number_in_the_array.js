0b54798e284cc01931b2ffde973f73f5e53a3703
txy <656877449@qq.com>
2020-10-21 12:39:06 +0800

    新增或修改

--- third_largest_number_in_the_array.js
+++ third_largest_number_in_the_array.js
@@ -1,11 +1,25 @@
 function getNum(arr) {
-  var max = (arr[2] > arr[1] && arr[2] > arr[0] ? arr[2] : arr[1]);
-  var sec = (arr[1] > arr[0] && arr[2] > arr[1] ? arr[1] : arr[0]);
-  var thi = (arr[1] > arr[0] && arr[2] > arr[0] ? arr[0] : arr[1]);
+  var max = (arr[1] > arr[0] ? arr[1] : arr[0]);
+  var sec = (arr[1] > arr[0] ? arr[0] : arr[1]);
+  var thi = 0;
+  if (arr[2] > max) {
+    thi = sec;
+    sec = max;
+    max = arr[2];
+  } else if (arr[2] > sec && arr[2] < max) {
+    thi = sec;
+    sec = arr[2];
+  } else if (arr[2] < sec) {
+    thi = arr[2];
+  }
+
   for (var i = 3; i < arr.length; ++i) {
     if (arr[i] > max) {
+      thi = sec;
+      sec = max;
       max = arr[i];
     } else if (max > arr[i] && arr[i] > sec) {
+      thi = sec;
       sec = arr[i];
     } else if (sec > arr[i] && arr[i] > thi) {
       thi = arr[i];
