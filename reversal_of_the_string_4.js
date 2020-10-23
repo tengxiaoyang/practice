b6e373cac4e92d420e4a9399ed68c2b0faa243d5
txy <656877449@qq.com>
2020-10-23 14:52:27 +0800

    新增或修改

--- reversal_of_the_string_4.js
+++ reversal_of_the_string_4.js
@@ -1,6 +1,4 @@
 function reversal_of_the_string(str) {
-  let arr = str.split("");
-  let new_arr = arr.reverse();
-  let new_str = new_arr.join("");
+  let new_str = str.split("").reverse().join("");
   return new_str;
 }
