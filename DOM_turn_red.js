0e53402af34652c639dffefdbb30402fa0b6e929
txy <656877449@qq.com>
2020-10-26 13:32:51 +0800

    新增或修改

---
+++ DOM_turn_red.js
@@ -0,0 +1,47 @@
+var a_1 = document.getElementById('3001');
+a_1.querySelector("a").style.backgroundColor = "#f00";
+
+var a_2 = document.getElementById('3002');
+a_2.querySelector("a").style.backgroundColor = "#f00";
+
+var a_3 = document.getElementById('3003');
+a_3.querySelector("a").style.backgroundColor = "#f00";
+
+var a_4 = document.getElementById('3004');
+a_4.querySelector("a").style.backgroundColor = "#f00";
+
+var a_5 = document.getElementById('3005');
+a_5.querySelector("a").style.backgroundColor = "#f00";
+
+var r_1 = document.getElementById('1');
+r_1.querySelector("a").style.backgroundColor = "#f00";
+
+var r_2 = document.getElementById('2');
+r_2.querySelector("a").style.backgroundColor = "#f00";
+
+var r_3 = document.getElementById('3');
+r_3.querySelector("a").style.backgroundColor = "#f00";
+
+var r_4 = document.getElementById('4');
+r_4.querySelector("a").style.backgroundColor = "#f00";
+
+var r_5 = document.getElementById('5');
+r_5.querySelector("a").style.backgroundColor = "#f00";
+
+var r_6 = document.getElementById('6');
+r_6.querySelector("a").style.backgroundColor = "#f00";
+
+var r_7 = document.getElementById('7');
+r_7.querySelector("a").style.backgroundColor = "#f00";
+
+var r_8 = document.getElementById('8');
+r_8.querySelector("a").style.backgroundColor = "#f00";
+
+var r_9 = document.getElementById('9');
+r_9.querySelector("a").style.backgroundColor = "#f00";
+
+var r_10 = document.getElementById('10');
+r_10.querySelector("a").style.backgroundColor = "#f00";
+
+var r_11 = document.getElementById('11');
+r_11.querySelector("a").style.backgroundColor = "#f00";
