0d41f9c2a8762ab9b68b437bb121e9916974b97b
txy <656877449@qq.com>
2020-10-26 18:46:31 +0800

    新增或修改

--- count_down_circle.js
+++ count_down_circle.js
@@ -8,8 +8,6 @@ function time_circle() {
   if (circle_wait === 0) {
     circle_direction = -1;
     circle_wait = 1;
-  } else if (wait_3 === 101) {
-    wait_3 = 100;
   } else {
     circle_wait = circle_wait - 1*direction;
   }
