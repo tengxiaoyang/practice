f87a497c70efb7169040eff59a82821467a8a431
tengxiaoyang <656877449@qq.com>
2021-02-18 14:22:34 +0800

    add or modify

--- rmoveDir.js
+++ rmoveDir.js
@@ -4,7 +4,7 @@ function removeDir(path) {
   let data = fs.readdirSync(path); //data是一个数组，文件夹名和文件名用引号括起来，如["1", "2.txt", "3.html"]
 
   for (let i = 0; i < data.length; ++ i) {
-    //用循环判断数组中的元素是文件还是文件夹，是文件就删除，是目录就继续查找
+    //用循环判断数组中的元素是文件还是文件夹，是文件就删除，是文件夹就继续查找
     let url = path + "/" + data[i];
     let stat = fs.statSync(url); //用fs.stat获取文件或文件夹的详细信息
     if (stat.isDirectory()) {
